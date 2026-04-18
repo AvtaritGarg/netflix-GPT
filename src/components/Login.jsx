cbc056f6823365df75e0726d2a661f03d073f865
Avtarit Garg <avtaritgarg05@gmail.com>
2026-04-18 17:15:59 +0530

    added validation for form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,32 @@
-import { useState } from "react"
+import { useState, useRef  } from "react"
+import Header from "./Header"
+import { checkValidData } from "../utils/Validate"
 
 
 const Login = () => {
 
     const [isSignInForm, setIsSignInForm] = useState(true)
 
+    const [errorMessage, setErrorMessage] = useState("")
+    
+    const email = useRef(null)
+    const password = useRef(null)
+
     const toggleSignInForm = () =>{
         setIsSignInForm(!isSignInForm)
     }
+
+    const handleButtonClick = () => {
+        // Validate email and password
+        const message = checkValidData(email?.current?.value,password?.current?.value)
+        setErrorMessage(message)
+    }
+
     
 
     return (
         <div>
+            <Header/>
             <div className="absolute -z-10">
                 <img src="https://assets.nflxext.com/ffe/siteui/vlv3/7ea4545e-42d3-4ebf-82fd-0e1984dc6375/web/IN-en-20260316-TRIFECTA-perspective_789c5633-3949-4708-8e6c-8ddfd22ed696_large.jpg"
                 alt = "background"
@@ -21,7 +36,7 @@ const Login = () => {
 
             <div className="absolute bg-black my-44 mx-auto right-0 left-0 w-3/12 opacity-80 rounded-lg">
             
-            <form className="">
+            <form className="" onSubmit={(e)=> e.preventDefault()}>
                 <h1 className="text-white text-5xl p-4 pt-8 ml-6 font-bold cursor-default">
                     {isSignInForm? "Sign In":"Sign Up"}
                     </h1>
@@ -30,11 +45,27 @@ const Login = () => {
                     <input type="text" placeholder="Full Name" className="m-4 p-2 w-70 ml-11 rounded-lg bg-gray-400"/>
                 )
                 }
-                <input type="text" placeholder="Email Address" className= "rounded-lg bg-gray-400 m-4 p-2 w-70 ml-11"/>
-                <input type="text" placeholder="Password" className="m-4 p-2 w-70 ml-11 rounded-lg bg-gray-400"/>
+                <input
+                ref= {email}
+                 type="text" 
+                 placeholder="Email Address" 
+                 className= "rounded-lg bg-gray-400 m-4 p-2 w-70 ml-11"
+                 />
+                <input 
+                 ref = {password}
+                    type="password" 
+                    placeholder="Password" 
+                    className="m-4 p-2 w-70 ml-11 rounded-lg bg-gray-400"
+                    />
+
+                
+                <p className="text-red-600 mx-7 px-6 w-10/12 text-center">{errorMessage}</p>
 
 
-                <button className= "bg-red-800 w-70 m-4 p-2 ml-11 rounded-lg  cursor-pointer ">
+                <button className= "bg-red-800 w-70 m-4 p-2 ml-11 rounded-lg  cursor-pointer" 
+                    onClick={
+                        handleButtonClick
+                    }>
                     {isSignInForm? "Sign In": "Sign Up"}
                 </button>
                 <p className= "text-white opacity-100 ml-11 pt-4 pb-6 mb-8 cursor-pointer" onClick={toggleSignInForm}>{isSignInForm? "Already Registered? Sign In now ": "New to netflix? Sign Up Now"}</p>
--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import Header from './components/Header.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header/>
+    {/* <Header/> */}
     <Body />
   </StrictMode>,
 )
